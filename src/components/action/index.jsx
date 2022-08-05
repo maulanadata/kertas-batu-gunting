a9580c84fffbe9d3540499ef70b29a7c9079e030
Mohamad Wildan Maulana <63152884+maulanadata@users.noreply.github.com>
2022-08-06 00:09:20 +0700

    Update index.jsx

--- src/components/action/index.jsx
+++ src/components/action/index.jsx
@@ -1,40 +1,74 @@
-import React from "react";
-import { Card } from "antd";
-import { Button } from "antd";
-import "antd/dist/antd.css";
+import React, { useMemo, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Action = props => {
+function Action() {
+  const choices = ["rock", "paper", "scissors"];
+  const [selected, setSelected] = useState("");
+  const [computerSelected, setComputerSelected] = useState("");
+
+  const play = () => {
+    if (!selected) {
+      return ;
+    }
+    const computerChoiceIndex = Math.floor(Math.random() * choices.length);
+    setComputerSelected(choices[computerChoiceIndex]);
+  };
+
+  const result = useMemo(() => {
+    if (computerSelected === selected) {
+      return `Draw`;
+    } else {
+      if (
+        (computerSelected === "rock" && selected === "scissors") ||
+        (computerSelected === "paper" && selected === "rock") ||
+        (computerSelected === "scissors" && selected === "paper")
+      ) {
+        return "Computer won";
+      }
+      return "You won";
+    }
+  }, [computerSelected, selected]);
+
   return (
-    <Card title="Controls" style={{ width: "300px", height: "250px", alignItems: "center" }} >
-      <p style={{ alignItems: "center" }}>
-        <Button type="dashed" size="large" shape="round" block
-          onClick={() => props.wUpdate("r")}
-          disabled={!props.isActive}
-        >
-          Rock
-        </Button>
-      </p>
+    <div class="row mt-4 justify-content-center">
 
-      <p style={{ alignItems: "center" }}>
-        <Button type="dashed" size="large" shape="round" block
-          onClick={() => props.wUpdate("p")}
-          disabled={!props.isActive}
-        >
-          Paper
-        </Button>
-      </p>
-
-      <p style={{ alignItems: "center" }}>
-        <Button type="dashed" size="large" shape="round" block
-          onClick={() => props.wUpdate("s")}
-          disabled={!props.isActive}
-        >
-          Scissors
-        </Button>
-      </p>
-
-    </Card>
+      <div class="col-lg-3 d-flex align-items-stretch">
+        <div class="card text-center" style={{width: "300px"}}>
+          <div class="card-header mb-2"> You </div>
+          <div class="card-body">
+            <button class="btn btn-primary mb-2" onClick={() => setSelected("rock")}>Rock</button> <br />
+            <button class="btn btn-primary me-4" onClick={() => setSelected("paper")}>Paper</button>
+            <button class="btn btn-primary" onClick={() => setSelected("scissors")}>Scissors</button>
+          </div>
+          <div class="card-footer text-muted"> {selected} </div>
+        </div>
+  
+      </div>
+      
+      <div class="col-lg-3 d-flex align-items-stretch">
+        <div class="card text-center" style={{width: "300px"}}>
+          <div class="card-header mb-2"> Result </div>
+          <div class="card-body">
+            <button class="btn btn-primary" onClick={play}>play</button>
+          </div>
+          <div class="card-footer text-muted" id="hasil"> {result} </div>
+        </div>
+      </div>
+      
+      <div class="col-lg-3 d-flex align-items-stretch">
+        <div class="card text-center" style={{width: "300px"}}>
+          <div class="card-header mb-2"> Computer </div>
+          <div class="card-body">
+            <button class="btn btn-primary mb-2 disabled">Rock</button> <br />
+            <button class="btn btn-primary me-4 disabled">Paper</button>
+            <button class="btn btn-primary disabled">Scissors</button>
+          </div>
+          <div class="card-footer text-muted"> {computerSelected} </div>
+        </div>
+      </div>
+    </div>
+    
   );
-};
+}
 
 export default Action;
